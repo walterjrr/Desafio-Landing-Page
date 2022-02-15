8d3a6e1251c4d2bf546705ae4a7bfdf6ea3ca812
walterjrr <jrwalter731@gmail.com>
2022-02-15 14:30:44 -0300

    criando loja de produtos

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-const send = document.querySelector('#button-send')
+const productsContainer = document.querySelector(".products")
 
-const URL = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products"
+const url = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products"
 
-fetch(URL).then(function(res){ 
-  res.json().then(function(data){
-    console.log(data)
-  })
+fetch(url).then(function(res){
+    res.json().then(function(data){
+        let products = data.products
+
+        const list_products = products.map((item) => `
+            <div class="${item.id} list-products">
+                <img src="${item.image}" width="100px">
+                <div class="text-container">
+                    <h1>${item.name}</h2>
+                    <p>${item.description}</p>
+                    <h3>De ${item.oldPrice}R$ </h3>
+                    <h4>Para ${item.price}R$ </h4>
+                    <h5>ou ${item.installments.count} Vezes de ${item.installments.value}R$ </h5>
+                </div>    
+                <button>Comprar</button>
+            </div>
+        `
+        ).join('')
+
+        console.log(list_products)
+
+        productsContainer.innerHTML += list_products
+    })
+    
 })
-.catch(function(err){
-  console.error('Não foi possível achar a informação', err)
-})
-
 
 
